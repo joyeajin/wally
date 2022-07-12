e519cd216e41f607a8bd7cbe6138438994931a66
joyeajin <dpwls31200@gmail.com>
2022-07-12 11:53:29 +0900

    first commit

---
+++ js/wally.js
@@ -0,0 +1,27 @@
+const arrTop = document.querySelector('#arrTop');
+const arrBottom = document.querySelector('#arrBottom');
+const leg = document.querySelector('#leg');
+const face = document.querySelector('#face');
+const heightBox = document.querySelector('#heightBox');
+
+let currentHeight = 180;
+const checkPoint = 180;
+
+arrTop.addEventListener('click',()=>{
+    currentHeight+=5;
+    updateHeight();
+})
+arrBottom.addEventListener('click',()=>{
+    currentHeight-=5;
+    updateHeight();
+})
+
+function updateHeight(){
+    heightBox.innerText=currentHeight;
+    leg.style.height=currentHeight+'px';
+    if(checkPoint>currentHeight){
+        face.style.backgroundPosition='right';
+    }else{
+        face.style.backgroundPosition='left';
+    }
+}
